129c7b4f4facbff16b2c7411a98c6869d5037275
Nick Coghlan <ncoghlan@gmail.com>
2012-09-26 20:23:41 +1000

    Update Unicode size demo

--- talks/2012-pyconindia/py3.3-highlights/theme/demo-js/unicode_size.js
+++ talks/2012-pyconindia/py3.3-highlights/theme/demo-js/unicode_size.js
@@ -6,236 +6,286 @@
         58, 
         15
       ], 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
+      "term": "91923c473f0a4243857e416e0cc2fb16"
     }, 
     {
       "act": "PAUSE", 
-      "duration": 0.13050103187561035
+      "duration": 0.22466301918029785
     }, 
     {
       "act": "WRITE", 
-      "data": "\u001b]0;ncoghlan@lancre:~/devel/drafts/talks/pyconindia-2012/demo\u0007\u001b[?1034h[ncoghlan@lancre demo]$ ", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
+      "data": "\u001b]0;ncoghlan@lancre:~/devel/misc/talks/2012-pyconindia/py3.3-highlights/theme/demo-js\u0007\u001b[?1034h$", 
+      "term": "91923c473f0a4243857e416e0cc2fb16"
     }, 
     {
       "act": "ECHO", 
       "data": "python3.2", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
+      "term": "91923c473f0a4243857e416e0cc2fb16"
     }, 
     {
       "act": "READ", 
       "data": "\r", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
+      "term": "91923c473f0a4243857e416e0cc2fb16"
     }, 
     {
       "act": "WRITE", 
       "data": "\r\n", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
+      "term": "91923c473f0a4243857e416e0cc2fb16"
     }, 
     {
       "act": "PAUSE", 
-      "duration": 0.0447690486907959
+      "duration": 0.04568290710449219
     }, 
     {
       "act": "WRITE", 
       "data": "Python 3.2.3 (default, Jun  8 2012, 05:36:09) \r\n[GCC 4.7.0 20120507 (Red Hat 4.7.0-5)] on linux2\r\nType \"help\", \"copyright\", \"credits\" or \"license\" for more information.\r\n\u001b[?1034h>>> ", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
+      "term": "91923c473f0a4243857e416e0cc2fb16"
     }, 
     {
       "act": "ECHO", 
       "data": "import sys", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
+      "term": "91923c473f0a4243857e416e0cc2fb16"
     }, 
     {
       "act": "READ", 
       "data": "\r", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
+      "term": "91923c473f0a4243857e416e0cc2fb16"
     }, 
     {
       "act": "WRITE", 
       "data": "\r\n>>> ", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
+      "term": "91923c473f0a4243857e416e0cc2fb16"
     }, 
     {
       "act": "ECHO", 
-      "data": "sys.getsizeof(\"Hello world\")", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
+      "data": "\u0939\u0948\u0932\u094b_\u0926\u0941\u0928\u093f\u092f\u093e = \"\u0939\u0948\u0932\u094b \u0926\u0941\u0928\u093f\u092f\u093e\"", 
+      "term": "91923c473f0a4243857e416e0cc2fb16"
     }, 
     {
       "act": "READ", 
       "data": "\r", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
+      "term": "91923c473f0a4243857e416e0cc2fb16"
     }, 
     {
       "act": "WRITE", 
-      "data": "\r\n104\r\n>>> ", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
+      "data": "\r\n>>> ", 
+      "term": "91923c473f0a4243857e416e0cc2fb16"
     }, 
     {
       "act": "ECHO", 
-      "data": "sys.getsizeof(\"\"", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
-    }, 
-    {
-      "act": "READ", 
-      "data": "\u007f", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
-    }, 
-    {
-      "act": "WRITE", 
-      "data": "\b\u001b[K", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
-    }, 
-    {
-      "act": "ECHO", 
-      "data": "\\u0939\\u0948\\u0932\\u094b \\u0926\\u0941\\u0928\\u093f\\u092f\\u093e\")", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
+      "data": "sys.getsizeof(\u0939\u0948\u0932\u094b_\u0926\u0941\u0928\u093f\u092f\u093e)", 
+      "term": "91923c473f0a4243857e416e0cc2fb16"
     }, 
     {
       "act": "READ", 
       "data": "\r", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
-    }, 
-    {
-      "act": "WRITE", 
-      "data": "\r\n104\r\n>>> ", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
-    }, 
-    {
-      "act": "READ", 
-      "data": "\u0004", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
+      "term": "91923c473f0a4243857e416e0cc2fb16"
     }, 
     {
       "act": "WRITE", 
       "data": "\r\n", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
+      "term": "91923c473f0a4243857e416e0cc2fb16"
     }, 
     {
-      "act": "PAUSE", 
-      "duration": 0.00016498565673828125
+      "act": "READ", 
+      "data": "\u0939", 
+      "term": "91923c473f0a4243857e416e0cc2fb16"
     }, 
     {
       "act": "WRITE", 
-      "data": "\u001b]0;ncoghlan@lancre:~/devel/drafts/talks/pyconindia-2012/demo\u0007[ncoghlan@lancre demo]$ ", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
+      "data": "104\r\n>>> \u0939", 
+      "term": "91923c473f0a4243857e416e0cc2fb16"
+    }, 
+    {
+      "act": "ECHO", 
+      "data": "\u0948\u0932\u094b_\u0926\u0941\u0928\u093f\u092f\u093e = \"Hello world\"", 
+      "term": "91923c473f0a4243857e416e0cc2fb16"
+    }, 
+    {
+      "act": "READ", 
+      "data": "\r", 
+      "term": "91923c473f0a4243857e416e0cc2fb16"
+    }, 
+    {
+      "act": "WRITE", 
+      "data": "\r\n>>> ", 
+      "term": "91923c473f0a4243857e416e0cc2fb16"
+    }, 
+    {
+      "act": "ECHO", 
+      "data": "sys.getsizeof(\u0939\u0948\u0932\u094b_\u0926\u0941\u0928\u093f\u092f\u093e)", 
+      "term": "91923c473f0a4243857e416e0cc2fb16"
+    }, 
+    {
+      "act": "READ", 
+      "data": "\r", 
+      "term": "91923c473f0a4243857e416e0cc2fb16"
+    }, 
+    {
+      "act": "WRITE", 
+      "data": "\r\n104\r\n>>> ", 
+      "term": "91923c473f0a4243857e416e0cc2fb16"
+    }, 
+    {
+      "act": "READ", 
+      "data": "\u0004", 
+      "term": "91923c473f0a4243857e416e0cc2fb16"
+    }, 
+    {
+      "act": "WRITE", 
+      "data": "\r\n", 
+      "term": "91923c473f0a4243857e416e0cc2fb16"
+    }, 
+    {
+      "act": "PAUSE", 
+      "duration": 0.005072116851806641
+    }, 
+    {
+      "act": "WRITE", 
+      "data": "\u001b]0;ncoghlan@lancre:~/devel/misc/talks/2012-pyconindia/py3.3-highlights/theme/demo-js\u0007$", 
+      "term": "91923c473f0a4243857e416e0cc2fb16"
     }, 
     {
       "act": "ECHO", 
       "data": "~/devel/py3k/python", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
+      "term": "91923c473f0a4243857e416e0cc2fb16"
     }, 
     {
       "act": "READ", 
       "data": "\r", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
+      "term": "91923c473f0a4243857e416e0cc2fb16"
     }, 
     {
       "act": "WRITE", 
       "data": "\r\n", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
+      "term": "91923c473f0a4243857e416e0cc2fb16"
     }, 
     {
       "act": "PAUSE", 
-      "duration": 0.0254209041595459
+      "duration": 0.02636408805847168
     }, 
     {
       "act": "WRITE", 
       "data": "Python 3.3.0rc2+ (default:178f9042af81, Sep 25 2012, 20:06:17) \r\n[GCC 4.7.0 20120507 (Red Hat 4.7.0-5)] on linux\r\nType \"help\", \"copyright\", \"credits\" or \"license\" for more information.\r\n\u001b[?1034h>>> ", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
+      "term": "91923c473f0a4243857e416e0cc2fb16"
     }, 
     {
       "act": "ECHO", 
       "data": "import sys", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
+      "term": "91923c473f0a4243857e416e0cc2fb16"
     }, 
     {
       "act": "READ", 
       "data": "\r", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
+      "term": "91923c473f0a4243857e416e0cc2fb16"
     }, 
     {
       "act": "WRITE", 
       "data": "\r\n>>> ", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
+      "term": "91923c473f0a4243857e416e0cc2fb16"
     }, 
     {
       "act": "ECHO", 
-      "data": "sys.getsizeof(\"Hello world\")", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
+      "data": "\u0939\u0948\u0932\u094b_\u0926\u0941\u0928\u093f\u092f\u093e = \"\u0939\u0948\u0932\u094b \u0926\u0941\u0928\u093f\u092f\u093e\"", 
+      "term": "91923c473f0a4243857e416e0cc2fb16"
     }, 
     {
       "act": "READ", 
       "data": "\r", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
-    }, 
-    {
-      "act": "WRITE", 
-      "data": "\r\n60\r\n>>> ", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
-    }, 
-    {
-      "act": "ECHO", 
-      "data": "sys.getsizeof(\"\\u0939\\u0948\\u0932\\u094b \\u0926\\u0941\\u0928\\u093f\\u092f\\u093e\")", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
-    }, 
-    {
-      "act": "READ", 
-      "data": "\r", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
-    }, 
-    {
-      "act": "WRITE", 
-      "data": "\r\n96\r\n>>> ", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
-    }, 
-    {
-      "act": "ECHO", 
-      "data": "\"\\u0939\\u0948\\u0932\\u094b \\u0926\\u0941\\u0928\\u093f\\u092f\\u093e\"", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
-    }, 
-    {
-      "act": "READ", 
-      "data": "\r", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
-    }, 
-    {
-      "act": "WRITE", 
-      "data": "\r\n'\u0939\u0948\u0932\u094b \u0926\u0941\u0928\u093f\u092f\u093e'\r\n>>> ", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
-    }, 
-    {
-      "act": "READ", 
-      "data": "\u0004", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
+      "term": "91923c473f0a4243857e416e0cc2fb16"
     }, 
     {
       "act": "WRITE", 
       "data": "\r\n", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
+      "term": "91923c473f0a4243857e416e0cc2fb16"
     }, 
     {
-      "act": "PAUSE", 
-      "duration": 0.00024318695068359375
+      "act": "READ", 
+      "data": "s", 
+      "term": "91923c473f0a4243857e416e0cc2fb16"
     }, 
     {
       "act": "WRITE", 
-      "data": "\u001b]0;ncoghlan@lancre:~/devel/drafts/talks/pyconindia-2012/demo\u0007[ncoghlan@lancre demo]$ ", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
+      "data": ">>> sys.gets", 
+      "term": "91923c473f0a4243857e416e0cc2fb16"
+    }, 
+    {
+      "act": "ECHO", 
+      "data": "izeof(\u0939\u0948\u0932\u094b_\u0926\u0941\u0928\u093f\u092f\u093e)", 
+      "term": "91923c473f0a4243857e416e0cc2fb16"
+    }, 
+    {
+      "act": "READ", 
+      "data": "\r", 
+      "term": "91923c473f0a4243857e416e0cc2fb16"
+    }, 
+    {
+      "act": "WRITE", 
+      "data": "\r\n96\r\n>>> ", 
+      "term": "91923c473f0a4243857e416e0cc2fb16"
+    }, 
+    {
+      "act": "ECHO", 
+      "data": "\u0939\u0948\u0932\u094b_\u0926\u0941\u0928\u093f\u092f\u093e = \"Hello world\"", 
+      "term": "91923c473f0a4243857e416e0cc2fb16"
+    }, 
+    {
+      "act": "READ", 
+      "data": "\r", 
+      "term": "91923c473f0a4243857e416e0cc2fb16"
+    }, 
+    {
+      "act": "WRITE", 
+      "data": "\r\n>>> ", 
+      "term": "91923c473f0a4243857e416e0cc2fb16"
+    }, 
+    {
+      "act": "ECHO", 
+      "data": "sys.getsizeof(\u0939\u0948\u0932\u094b_\u0926\u0941\u0928\u093f\u092f\u093e)", 
+      "term": "91923c473f0a4243857e416e0cc2fb16"
+    }, 
+    {
+      "act": "READ", 
+      "data": "\r", 
+      "term": "91923c473f0a4243857e416e0cc2fb16"
+    }, 
+    {
+      "act": "WRITE", 
+      "data": "\r\n60\r\n>>> ", 
+      "term": "91923c473f0a4243857e416e0cc2fb16"
     }, 
     {
       "act": "READ", 
       "data": "\u0004", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
+      "term": "91923c473f0a4243857e416e0cc2fb16"
     }, 
     {
       "act": "WRITE", 
-      "data": "exit\r\n", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
+      "data": "\r\n", 
+      "term": "91923c473f0a4243857e416e0cc2fb16"
+    }, 
+    {
+      "act": "PAUSE", 
+      "duration": 0.0041351318359375
+    }, 
+    {
+      "act": "WRITE", 
+      "data": "\u001b]0;ncoghlan@lancre:~/devel/misc/talks/2012-pyconindia/py3.3-highlights/theme/demo-js\u0007$", 
+      "term": "91923c473f0a4243857e416e0cc2fb16"
+    }, 
+    {
+      "act": "READ", 
+      "data": "\u0004", 
+      "term": "91923c473f0a4243857e416e0cc2fb16"
+    }, 
+    {
+      "act": "WRITE", 
+      "data": "<Press Enter to close demo>\r\n",
+      "term": "91923c473f0a4243857e416e0cc2fb16"
     }, 
     {
       "act": "CLOSE", 
-      "term": "6eaf86f144b94c63b8eda196dc3eabe5"
+      "term": "91923c473f0a4243857e416e0cc2fb16"
     }
   ]
 }
