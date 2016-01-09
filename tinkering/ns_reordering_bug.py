fe51cd3dc45fa1b7970cd1d2b40d209ea3caf469
Nick Coghlan <ncoghlan@gmail.com>
2016-01-09 21:32:36 +1000

    Tinker with bug found by Dave Beazley

---
+++ tinkering/ns_reordering_bug.py
@@ -0,0 +1,39 @@
+# Bug where class keys get reordered after creation when replacing values
+# See https://gist.github.com/dabeaz/617a5b0542d57e003433 for context
+
+def decorate(func):
+    print('Decorating', func.__name__)
+    return func
+
+def wrap_methods(cls):
+    for name in vars(cls):
+        if name.startswith('f_'):
+            setattr(cls, name, decorate(getattr(cls, name)))
+    return cls
+
+class Meta(type):
+    pass
+
+# Uncomment below to try the callable-as-metaclass path
+# Meta = lambda *args, **kwds: type(*args, **kwds)
+
+class Spam(metaclass=Meta):
+    def f_1(self): pass
+    def f_2(self): pass
+    def f_3(self): pass
+    def f_4(self): pass
+    def f_5(self): pass
+    def f_6(self): pass
+    def f_7(self): pass
+    def f_8(self): pass
+
+attributes = list(Spam.__dict__)
+print(attributes)
+
+wrap_methods(Spam)
+
+wrapped_attributes = list(Spam.__dict__)
+print(wrapped_attributes)
+
+print("Broken = ", attributes != wrapped_attributes)
+
